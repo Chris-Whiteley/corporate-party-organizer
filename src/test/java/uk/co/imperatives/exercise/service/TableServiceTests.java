140d1e808cbbf102f0597d0a6b60d8ee0c2b6827
Chris Whiteley <chris.whiteley81@gmail.com>
2024-09-28 19:15:00 +0100

    test: added failing test for adding a table with a given table number and specified number of seats.

--- src/test/java/uk/co/imperatives/exercise/service/TableServiceTests.java
+++ src/test/java/uk/co/imperatives/exercise/service/TableServiceTests.java
@@ -34,12 +34,37 @@ public class TableServiceTests {
                 table.getNumber() == null  &&
                         table.getNoOfSeats() == tableToAdd.getNoOfSeats() &&
                         table.getNoOfSeatsAllocated() == tableToAdd.getNoOfSeatsAllocated() &&
-                        table.getVersion()== null // matching null version field
+                        table.getVersion()== null
         ))).thenReturn(returnedTable);
 
         // Call the service to add the table
         var noOfSeats = 6;
         Table result = tableService.addTable(noOfSeats);
+
+        // Validate the result
+        assertNotNull(result);
+        assertEquals(returnedTable.getNumber(), result.getNumber());
+        assertEquals(returnedTable.getNoOfSeats(), result.getNoOfSeats());
+        assertEquals(returnedTable.getNoOfSeatsAllocated(), result.getNoOfSeatsAllocated());
+        assertEquals(returnedTable.getVersion(), result.getVersion());
+    }
+
+    @Test
+    void shouldAddTableWithGivenTableNumber() {
+        Table tableToAdd = Table.builder().number(10).noOfSeats(4).noOfSeatsAllocated(0).build();
+        Table returnedTable = Table.builder().number(10).noOfSeats(4).noOfSeatsAllocated(0).version(0L).build();
+
+        when(tableRepository.save(argThat(table ->
+                table.getNumber() == 10  &&
+                        table.getNoOfSeats() == tableToAdd.getNoOfSeats() &&
+                        table.getNoOfSeatsAllocated() == tableToAdd.getNoOfSeatsAllocated() &&
+                        table.getVersion()== null
+        ))).thenReturn(returnedTable);
+
+        // Call the service to add the table
+        var tableNumber = 4;
+        var noOfSeats = 4;
+        Table result = tableService.addTable(tableNumber, noOfSeats);
 
         // Validate the result
         assertNotNull(result);
